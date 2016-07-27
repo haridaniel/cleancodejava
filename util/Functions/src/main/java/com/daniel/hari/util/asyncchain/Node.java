cfe5b17bbc056951498d7d8db29c2d2c50bdde7c
Hári Dániel <haridaniel0@gmail.com>
2016-07-27 18:55:40 +0200

    Renamed project

--- util/Functions/src/main/java/com/daniel/hari/util/asyncchain/Node.java
+++
@@ -1,7 +0,0 @@
-package com.daniel.hari.util.asyncchain;
-
-import com.daniel.hari.util.function.AsyncFunction;
-
-interface Node<T, R> extends AsyncFunction<T, R> {
-	void chain(AsyncFunction<T, R> target);
-}
--- util/Functions/src/main/java/com/daniel/hari/util/function/NullConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.daniel.hari.util.function;
-
-
-/** Do nothing. **/
-public class NullConsumer<T> implements Consumer<T> {
-	@Override
-	public void accept(T t) {}
-}
